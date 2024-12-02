c2fbec6b0cfebdd61d20f6b7f46844f279a6102a
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-12-02 15:52:51 -0300

    change verification

--- docs.py
+++ docs.py
@@ -9,10 +9,15 @@ def run_terraform_docs(directory):
                     # Comando a ser executado
                     command = ["terraform-docs", "-c", f"{root}/.docs/.terraform-docs.yml", f"{root}"]
                     print(f"Executando terraform-docs no {root}")
-                    with open(f"{root}/README.md", "w") as output_file:
+                    with open(f"{root}/README.md", "r+") as output_file:
                         # Executa o comando e redireciona a saída para o arquivo
-                        subprocess.run(command, stdout=output_file, stderr=subprocess.PIPE, text=True, check=True)
-                        print("README.md gerado com sucesso!")
+                        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+                        current_content = output_file.read()
+                        if current_content != result.stdout:
+                            output_file.write(result.stdout)
+                            print("README.md gerado com sucesso!")
+                        else:
+                            print("O conteúdo do README.md já está atualizado.")
                 except subprocess.CalledProcessError as e:
                     print("Erro ao executar o comando terraform-docs:")
                     print(e.stderr)
